6ba379d88441bd67b5010681427278952f30d253
Adnan Hossain <108882526+engadnanhossain@users.noreply.github.com>
2022-12-15 12:40:12 +0600

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,17 +1,22 @@
-from _tkinter import *
-from math import *
-num1 = float(input("Enter First Number: "))
-op = str(input("Enter Your Operator: "))
-num2 = float(input("Enter Second Number: "))
+while True:
+    choice = input("Please On your Calculator? (on): ")
+    if choice in 'on':
+        num1 = float(input("Enter First Number: "))
+        op = str(input("Enter Your Operator: "))
+        num2 = float(input("Enter Second Number: "))
+        if op == '+':
+            print("The Summation is= ", num1 + num2)
+        elif op == '-':
+            print("The Subtraction is= ", num1 - num2)
+        elif op == '*':
+            print("The Multiplication is= ", num1 * num2)
+        elif op == '/':
+            print("The Divider is= ", num1 / num2)
+        else:
+            print("Invalid Operator")
 
-
-if op == '+':
-    print("The Summation is= ",num1+num2)
-elif op == '-':
-    print("The Subtraction is= ", num1-num2)
-elif op == '*':
-    print("The Multiplication is= ", num1 * num2)
-elif op == '/':
-    print("The Divider is= ", num1 / num2)
-else:
-    print("Please Enter valid input")
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == 'no':
+            break
+    else:
+        print("Invalid")
